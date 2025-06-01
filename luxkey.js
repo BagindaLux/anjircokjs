7d339e80ebcf6fad188ebddacbe6488c228826d9
BagindaLux <luxzoke@gmail.com>
2025-06-01 11:24:33 +0700

    Create luxkey.js

---
+++ luxkey.js
@@ -0,0 +1,9 @@
+{
+  "SecretCode": "luxsence1",
+  "description": "OnlyPrem Jink",
+  "lastUpdated": "01-06-2025",
+  "author": "BagindaLux",
+  "status": "Active",
+  "lisence": "MIT",
+  "version": "1.0.0",
+}
